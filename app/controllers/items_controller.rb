2f18c14f980d9ec7c41a94b141a280c185fa4ee4
Makoto Uchida <makoto_uchida918@yahoo.co.jp>
2017-05-29 03:13:47 +0000

    want

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -13,11 +13,15 @@ class ItemsController < ApplicationController
       })
 
       results.each do |result|
-        # 扱い易いように Item としてインスタンスを作成する（保存はしない）
-        item = Item.new(read(result))
+        item = Item.find_or_initialize_by(read(result))
         @items << item
       end
     end
+  end
+  
+  def show
+    @item = Item.find(params[:id])
+    @want_users = @item.want_users
   end
 
   private
